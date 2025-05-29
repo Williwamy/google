b585164f760a30f30ddb691ce516d5ac9ede27cc
Williwamy <williamgarfield999@gmail.com>
2025-05-29 07:14:05 -0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.xbox.com/en-us/play";//your website url
+  let target = "https://luna.amazon.com/game/fortnite";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
