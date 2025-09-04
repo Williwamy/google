02d91ad2ed3cd8cdbe92cf2b6c6f6dce4e5d8cad
Williwamy <williamgarfield999@gmail.com>
2025-09-04 17:53:17 -0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "work.quiz.gamersuk.org";//your website url
+  let target = "https://level.utopia.hktrustco.hk/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
